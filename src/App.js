3b31a1c4e8d86bb47ff692f12446335128de0558
Dell <sangyeop0529@gmail.com>
2023-04-23 16:19:35 +0900

    react start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const name = "Dell";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="orange">Hello!</h1>
+      <h2>Hello!!</h2>
+      <p>My name is {name}</p>
+      <ul>
+        <li>우유</li>
+        <li>딸기</li>
+        <li>바나나</li>
+      </ul>
+      <img
+        style={{ height: "200px" }}
+        src="https://plus.unsplash.com/premium_photo-1680268643503-a9956959e8f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+        alt="img"
+      />
+    </>
   );
 }
 
