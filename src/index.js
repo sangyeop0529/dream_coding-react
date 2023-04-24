281c82404b2bbdd3fb1c010134f411d7ec11fda5
Dell <sangyeop0529@gmail.com>
2023-04-24 12:36:36 +0900

    mouse event

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import AppJSX from "./AppJSX";
-import AppProfile from "./AppProfile";
-import AppCounter from "./AppCounter";
-import AppProducts from "./AppProducts";
+import AppXY from "./AppXY";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppProducts />
+    <AppXY />
   </React.StrictMode>
 );
