3cff072db2d6d207d35583ceb765a20c5d57cc2f
Dell <sangyeop0529@gmail.com>
2023-04-25 11:45:05 +0900

    basic 완성

--- src/index.js
+++ src/index.js
@@ -10,10 +10,11 @@ import AppCard from "./AppCard";
 import AppTheme from "./AppTheme";
 import AppMentorsButton from "./AppMontorsButton";
 import AppProducts from "./basic/AppProducts";
+import AppClass from "./AppClass";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppProducts />
+    <AppClass />
   </React.StrictMode>
 );
